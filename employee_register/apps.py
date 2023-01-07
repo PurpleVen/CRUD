eebc7e33336dbf0f8814635cff37e42088cfa932
PurpleVen <vendra0408@gmail.com>
2023-01-07 21:08:27 +0530

    done till migration of models

--- employee_register/apps.py
+++ employee_register/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EmployeeRegConfig(AppConfig):
+class EmployeeRegisterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Employee_reg'
+    name = 'employee_register'
